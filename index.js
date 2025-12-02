3705ae224f66a32f47f817dd5daaeed55159ba2c
Isabela <isabelamarques046@gmail.com>
2025-12-02 16:51:55 -0300

    adicionando as rotas privadas

---
+++ index.js
@@ -0,0 +1,120 @@
+require("dotenv").config();
+
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const {Paciente} = require('./models');
+
+// Rota de Login para gerar o token
+app.post('/login', (req, res) => {
+    const { usuario, senha } = req.body;
+
+    const user = usuarios.find(u => u.usuario === usuario);
+
+    if (!user || user.senha !== senha) {
+        return res.status(401).json({ mensagem: 'Usuário ou senha do paciente inválidos.' });
+    }
+
+    const token = jwt.sign({ userId: user.id, usuario: user.usuario }, JWT_SECRET, { expiresIn: '20m' }); 
+
+    res.json({ mensagem: 'Login do paciente bem-sucedido!', token: token });
+});
+
+// Middleware para verificação do JWT
+function verificaJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ mensagem: 'Token de autenticação não fornecido.' });
+    }
+    
+    const token = authHeader.split(' ')[1]; 
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) {
+            if (err.name === 'TokenExpiredError') {
+                return res.status(401).json({ mensagem: 'Token expirado.' });
+            }
+            return res.status(401).json({ mensagem: 'Token inválido.' });
+        }
+        
+        req.usuarioLogado = decoded;
+        next(); 
+    });
+}
+
+app.get('/pacientes', verificaJWT, async (req, res) => {
+    try {
+        console.log('Acessado por:', req.usuarioLogado);
+
+        const listaPacientes = await Paciente.findAll(); 
+
+        res.json({
+            mensagem: `Lista de pacientes recuperada.`,
+            pacientes: listaPacientes
+        });
+    } catch (error) {
+        console.error('Erro ao buscar pacientes:', error);
+        res.status(500).json({ mensagem: 'Erro do servidor.' });
+    }
+});
+
+app.get('/pacientes/:id', async (req, res) => {
+    try {
+        const paciente = await Paciente.findByPk(req.params.id); 
+        if (!paciente) {
+            return res.status(404).json({ mensagem: 'Paciente não encontrado.' });
+        }
+        res.status(200).json(paciente);
+    } catch (error) {
+        res.status(500).json({ mensagem: 'Erro ao buscar paciente.' });
+    }
+});
+
+app.post('/pacientes', async (req, res) => {
+    try {
+        const novoPaciente = await Paciente.create(req.body);
+        res.status(201).json(novoPaciente);
+    } catch (error) {
+        res.status(400).json({ mensagem: 'Erro ao criar paciente.', erro: error.message });
+    }
+});
+
+//Atualizar um paciente 
+app.put('/pacientes/:id', async (req, res) => {
+    try {
+        const paciente = await Paciente.findByPk(req.params.id);
+        if (!paciente) {
+            return res.status(404).json({ mensagem: 'Paciente não encontrado.' });
+        }
+        await paciente.update(req.body);
+        res.status(200).json(paciente);
+    } catch (error) {
+        res.status(400).json({ mensagem: 'Erro ao atualizar paciente.', erro: error.message });
+    }
+});
+
+//Deletar um paciente
+app.delete('/pacientes/:id', async (req, res) => {
+    try {
+        const paciente = await Paciente.findByPk(req.params.id);
+        if (!paciente) {
+            return res.status(404).json({ mensagem: 'Paciente não encontrado.' });
+        }
+        await paciente.destroy();
+        res.status(204).send(); 
+    } catch (error) {
+        res.status(500).json({ mensagem: 'Erro ao deletar paciente.' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
